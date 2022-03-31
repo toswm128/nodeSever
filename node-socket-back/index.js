5029590d6e684544a6632411191962d80f213fb7
조민수 <chominsu10@gmail.com>
2022-03-31 12:11:06 +0900

    [ADD] socket connect

--- node-socket-back/index.js
+++ node-socket-back/index.js
@@ -2,9 +2,32 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const cors = require("cors");
+app.use(cors());
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3001",
+    methods: ["GET", "POST"],
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
+});
+
+io.on("connection", socket => {
+  socket.on("enter", (msg, done) => {
+    console.log(msg);
+    done();
+  });
+
+  socket.on("disconnect", () => {
+    console.log("she's gone");
+  });
+
+  console.log("a user connected");
 });
 
 server.listen(3030, () => {
