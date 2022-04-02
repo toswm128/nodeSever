718686549ccad0b21195e8956993e7e926894d14
조민수 <chominsu10@gmail.com>
2022-04-02 19:57:42 +0900

    [ADD] userMedia

--- node-socket-front/src/App.jsx
+++ node-socket-front/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import io from "socket.io-client";
 
 const socket = io("http://10.80.163.234:3030/");
@@ -6,6 +6,8 @@ function App() {
   const [text, setText] = useState("");
   const [name, setName] = useState("");
   const [chatList, setChatList] = useState([]);
+  const divRef = useRef();
+  const myStream = useRef(null);
   useEffect(() => {
     socket.on("post", data => {
       console.log(...chatList);
@@ -16,9 +18,24 @@ function App() {
     };
   }, []);
 
+  const getMedia = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true,
+      });
+      myStream.current.srcObject = stream;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  getMedia();
+
   return (
     <div className="App">
       <h1>My chat room</h1>
+      <video ref={myStream} autoPlay playsInline width="400"></video>
       <input
         type="text"
         placeholder="이름을 입력해 주세요"
@@ -45,7 +62,7 @@ function App() {
       </form>
       <div>
         {chatList.map(({ name, text }, key) => (
-          <div key={key}>
+          <div ref={key ? divRef : null} key={key}>
             {name}: {text}
           </div>
         ))}
